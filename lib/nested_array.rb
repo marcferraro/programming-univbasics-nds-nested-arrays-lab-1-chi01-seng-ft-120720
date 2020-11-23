2341e8589f02fea55c9af7ff502faabdb8afa1d5
Marc Ferraro <marc.ferraro1@gmail.com>
2020-11-23 20:45:06 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,6 +35,10 @@ end
 def matrix_lookup(matrix, row, column)
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
+  
+  sorted_matrix
+  sorted_matrix [0][1]
+  
 end
 
 def matrix_update(matrix, row, column, new_value)
